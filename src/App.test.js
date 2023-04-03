85bfcb68e3011f716b5a8aa5d92cd6cb9f0c241d
Laur <laurentiuvieru50@gmail.com>
2023-04-03 12:20:41 +0300

    testing-fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,24 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { renderWithProviders } from './test-utils';
 import Home from './components/Home/Home';
 import Add from './components/Add/Add';
 
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Home component', () => {
 
-
   test('should render elements on page', () => {
     renderWithProviders(<Home />)
-    expect(screen.getByText('Showcase your work with us')).toBeInTheDocument();
+    // expect(screen.getByText('Showcase your work with us')).toBeInTheDocument();
+
+    const homeContainer = screen.getByTestId("home-container");
+    const heading = screen.getByText("Showcase your work with us");
+
+    expect(homeContainer).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
   })
 
 })
@@ -24,35 +29,51 @@ describe('Home component', () => {
 describe('Add Component', () => {
 
   it('should render inputs and buttons', () => {
-    renderWithProviders(
-      <Router>
+    const { getByTestId } = renderWithProviders(
+      <MemoryRouter>
         <Add />
-      </Router>
+      </MemoryRouter>
     );
+
+    const titleInput = getByTestId('input-title');
+    const textarea = getByTestId('textarea');
+    const url = getByTestId('input-url');
+    const file = getByTestId('input-file');
+    const addBtn = getByTestId('submit-add-btn');
+    expect(titleInput).toBeTruthy();
+    expect(textarea).toBeTruthy();
+    expect(url).toBeTruthy();
+    expect(file).toBeTruthy();
+    expect(addBtn).toBeTruthy();
   })
-  // let getByTestId;
 
-  // beforeEach(() => {
-  //   ({ getByTestId } = renderWithProviders(
-  //     <Router>
-  //       <Add />
-  //     </Router>));
-  // });
+  it('should open file-selector when click "Choose File" button', () => {
+    const { getByTestId } = renderWithProviders(
+      <MemoryRouter>
+        <Add />
+      </MemoryRouter>
+    );
 
-  // it('should render inputs and buttons', () => {
-  //   // const titleInput = getByTestId('input-title');
-  //   // const textarea = getByTestId('textarea');
-  //   // const url = getByTestId('input-url');
-  //   // const file = getByTestId('input-file');
-  //   const addBtn = getByTestId('submit-add-btn');
-  //   // expect(titleInput).toBeTruthy();
-  //   // expect(textarea).toBeTruthy();
-  //   // expect(url).toBeTruthy();
-  //   // expect(file).toBeTruthy();
-  //   expect(addBtn).toBeTruthy();
-  // })
+    const chooseFileBtn = getByTestId('input-file');
+    expect(chooseFileBtn).toBeTruthy();
+    expect(chooseFileBtn).toBeInTheDocument();
+    fireEvent.click(chooseFileBtn);
 
+  })
+
+  it('should add project in projects after pressing submit button', () => {
+    const { getByTestId } = renderWithProviders(
+      <MemoryRouter>
+        <Add />
+      </MemoryRouter>
+    );
+
+
+    const submitButton = getByTestId('submit-add-btn');
+    expect(submitButton).toBeTruthy();
+    // fireEvent.click(submitButton);
+    expect(screen.getByText('Add Project')).toBeInTheDocument();
+  })
 })
-
 
 
